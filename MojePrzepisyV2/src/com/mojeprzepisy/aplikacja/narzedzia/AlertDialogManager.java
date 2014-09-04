292b6a7e3959b5ce127349513ce38b2e9cfb236d
redione193@gmail.com <redione193@gmail.com@ec91ace4-fb45-94ec-143e-0ac2999cc60f>
2014-09-04 15:13:54 +0000

    Dodano autmatyczne logowanie

--- MojePrzepisyV2/src/com/mojeprzepisy/aplikacja/narzedzia/AlertDialogManager.java
+++ MojePrzepisyV2/src/com/mojeprzepisy/aplikacja/narzedzia/AlertDialogManager.java
@@ -1,9 +1,12 @@
 package com.mojeprzepisy.aplikacja.narzedzia;
 
+import com.mojeprzepisy.aplikacja.R;
+
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 
 public class AlertDialogManager {
@@ -23,7 +26,8 @@ public class AlertDialogManager {
 	}
 
 	@SuppressWarnings("deprecation")
-	public void showUpdateDialog(final Context context, String title, String message) {
+	public void showUpdateDialog(final Context context, String title,
+			final String message) {
 		final AlertDialog alertDialog = new AlertDialog.Builder(context)
 				.create();
 		alertDialog.setTitle(title);
@@ -31,7 +35,15 @@ public class AlertDialogManager {
 
 		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which) {
-				Uri uri = Uri.parse("market://details?id=com.mojeprzepisy.aplikacja");
+				if (message.equals(context.getString(R.string.ocen_aplikacje))) {
+					SharedPreferences ocen = context.getSharedPreferences(
+							"ocen", 0);
+					SharedPreferences.Editor edytorPref = ocen.edit();
+					edytorPref.putBoolean("oceniono", true);
+					edytorPref.commit();
+				}
+				Uri uri = Uri
+						.parse("market://details?id=com.mojeprzepisy.aplikacja");
 				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 				context.startActivity(intent);
 				alertDialog.dismiss();
